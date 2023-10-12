86a42def5079b45a850279c6da76f935dbc632bb
Jonathan Peck <jonathan.peck@ugent.be>
2023-10-12 13:28:19 +0200

    more tests

--- tests/test_shearlets.py
+++ tests/test_shearlets.py
@@ -37,6 +37,11 @@ def test_inverse(shearletSystem, device):
 def test_call(shearletSystem, device):
     """Validate the regular call."""
 
+    def compute_psnr(img, x_hat):
+        mse = np.mean((img.cpu().numpy() - x_hat.cpu().numpy()) ** 2)
+        psnr = 20 * np.log10(255 / np.sqrt(mse))
+        return psnr
+
     # load data
     sigma = 25
     img = torch.from_numpy(np.array(Image.open('tests/barbara.jpg')).reshape(1, 1, 512, 512)).to(device)
@@ -62,13 +67,15 @@ def test_call(shearletSystem, device):
         x_hat = shearletSystem.reconstruct(new_coeffs)
 
         # compute PSNR
-        mse = np.mean((img.cpu().numpy() - x_hat.cpu().numpy()) ** 2)
-        psnr = 20 * np.log10(255 / np.sqrt(mse))
-        psnrs.append(psnr)
+        psnr1 = compute_psnr(img, img + noise)
+        psnr2 = compute_psnr(img, x_hat)
+        psnrs.append(psnr2)
+
+        assert psnr1 < psnr2, f'PSNR of noisy image ({psnr1:.2f}) must be lower than PSNR of restored image ({psnr2:.2f})'
 
     # compute result
     mean_psnr = np.mean(psnrs)
     std_psnr = np.std(psnrs)
     print(f'PSNR: {mean_psnr:.2f} dB ({std_psnr:.2f})')
 
-    assert mean_psnr > 25, f'PSNR is too low: {psnr:.2f} < 25 dB'
+    assert mean_psnr > 25, f'PSNR is too low: {mean_psnr:.2f} < 25 dB'
